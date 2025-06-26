03e029ba32146db7003ba60c710231c6bf5a8a9b
shivani <shivaniw69@gmail.com>
2025-06-26 12:03:25 +0530

    app

--- reactappp/src/App.jsx
+++ reactappp/src/App.jsx
@@ -1,7 +1,7 @@
 
 import { useSelector,useDispatch } from "react-redux";
 import { useState } from "react";
-import { addTask,taskDelete } from "./todoSlice";
+import { addTask,taskDelete,taskComplete,taskInComplete } from "./todoSlice";
 const App=()=>{
   const data=useSelector(state=>state.todo.task);
   const dispatch=useDispatch();
@@ -13,9 +13,27 @@ const App=()=>{
       <>
        <tr>
         <td>{sno}</td>
-        <td>{key.work}</td>
+        <td>{ taskStatus? 
+        <>
+         <span style={{color:"red",textDecoration:"line-through"}}>
+            {key.work}
+          </span>
+        </>
+      }:{
+        <>
+        {key.work}
+        </>
+      }
+      </td>
+          
         <td>
           <span onClick={()=>{dispatch(taskDelete({id:key.id}))}}>delete</span>
+        </td>
+        <td>
+          <span onClick={()=>{dispatch(taskComplete({id:key.id}))}}>complete</span>
+        </td>
+        <td>
+          <span onClick={()=>{dispatch(taskInComplete({id:key.id}))}}>incomplete</span>
         </td>
        </tr>
       </>
@@ -25,13 +43,15 @@ const App=()=>{
     <>
      <h1>ToDo App!</h1>
      Enter Task: <input type="text" value={txt} onChange={(e)=>{setTxt(e.target.value)}}/><br/><br/>
-     <button onClick={()=>{dispatch(addTask({id:Date.now(),work:txt}))}}>Add</button>
+     <button onClick={()=>{dispatch(addTask({id:Date.now(),work:txt,taskStatus:false}))}}>Add</button>
      <br/>
      <table border="1" width="300px">
       <tr>
         <th>Sno</th>
         <th>Task</th>
         <th>Delete</th>
+        <th>Complete</th>
+        <th>Incomplete</th>
       </tr>
       {ans}
      </table>
