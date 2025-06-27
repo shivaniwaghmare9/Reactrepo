ce86f04a1827f4de33c454707f7c10ca55e88cd9
shivani <shivaniw69@gmail.com>
2025-06-28 00:15:17 +0530

    header

--- reactproject/src/components/Header.jsx
+++ reactproject/src/components/Header.jsx
@@ -6,19 +6,13 @@ const Header=()=>{
     
     return(
         <>
-         <div id="header">
-            <div> 
-                <h1 id="title">MINISTORE</h1> 
-            </div>
-            
-            <div>
-                <h6 className="h6">Become a<br/> suplier</h6>
-             <h6 className="h6">Investor<br/> Relations</h6>
-              
-             <h4><FaRegUserCircle /> <br/>profile</h4>
+         <div className="header">
+             <div>
+              <h4><FaRegUserCircle /> <br/>profile</h4>
+              <h4><FaShoppingCart /><br/>cart</h4> 
+             </div>
              
-            <h4><FaShoppingCart /><br/>cart</h4> 
-            </div>
+            
              
            </div>
         </>
