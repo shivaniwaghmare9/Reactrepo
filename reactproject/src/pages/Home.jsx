916bee3078729e1e43e961c67e591fab1987fc16
shivani <shivaniw69@gmail.com>
2025-06-28 00:54:49 +0530

    home

--- reactproject/src/pages/Home.jsx
+++ reactproject/src/pages/Home.jsx
@@ -7,23 +7,23 @@ import img3 from "../images/shoes3.jpg"
 const Home=()=>{
    return(
     <>
-     <Carousel activeIndex={index} onSelect={handleSelect}>
+     <Carousel >
       <Carousel.Item>
-        <img src={img1} alt="web" />
+        <img src={img1} alt="web" width="100%" height="600px"/>
         <Carousel.Caption>
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img src={img2} alt="web" />
+        <img src={img2} alt="web" width="100%" height="600px"/>
         <Carousel.Caption>
           <h3>Second slide label</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img src={img3} alt="web" />
+        <img src={img3} alt="web" width="100%" height="600px"/>
         <Carousel.Caption>
           <h3>Third slide label</h3>
           <p>
