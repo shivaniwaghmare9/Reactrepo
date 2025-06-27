ec35b6bb35980849c8897e005660d081b861463c
shivani <shivaniw69@gmail.com>
2025-06-28 00:40:45 +0530

    img

--- reactproject/src/pages/Home.jsx
+++ reactproject/src/pages/Home.jsx
@@ -1,26 +1,29 @@
 
 
 import Carousel from 'react-bootstrap/Carousel';
+import img1 from "../images/Shoestem.jpg"
+import img2 from "../images/shoes2.jpg"
+import img3 from "../images/shoes3.jpg"
 const Home=()=>{
    return(
     <>
      <Carousel activeIndex={index} onSelect={handleSelect}>
       <Carousel.Item>
-        <ExampleCarouselImage text="First slide" />
+        <img src={img1} alt="web" />
         <Carousel.Caption>
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <ExampleCarouselImage text="Second slide" />
+        <img src={img2} alt="web" />
         <Carousel.Caption>
           <h3>Second slide label</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <ExampleCarouselImage text="Third slide" />
+        <img src={img3} alt="web" />
         <Carousel.Caption>
           <h3>Third slide label</h3>
           <p>
