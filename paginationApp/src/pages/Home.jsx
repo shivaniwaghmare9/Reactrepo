c34744808372d1f89b3946990030f3da32484f9a
shivani <shivaniw69@gmail.com>
2025-06-27 12:41:06 +0530

    home

--- paginationApp/src/pages/Home.jsx
+++ paginationApp/src/pages/Home.jsx
@@ -1,10 +1,13 @@
+ import myimg from "../images/myimg6.jpg";
 
 const Home=()=>{
     return(
         <>
-         <h1>This is home page!!</h1>
+        <h1 id="head">Home Page!!!</h1>
+        <div>
+         <img src={myimg} id="img"/>
+        </div>
         </>
-
     )
 }
 export default Home;
