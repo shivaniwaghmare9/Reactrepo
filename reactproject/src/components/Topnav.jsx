f03a969843e4f1648a5e7cd990b6d8a1f85aebbd
shivani <shivaniw69@gmail.com>
2025-06-29 00:55:32 +0530

    json

--- reactproject/src/components/Topnav.jsx
+++ reactproject/src/components/Topnav.jsx
@@ -6,11 +6,12 @@ import { Link } from 'react-router-dom';
 const Topnav=()=>{
     return(
         <>
-         <Navbar bg="dark" data-bs-theme="dark">
+         <Navbar bg="dark" data-bs-theme="dark" id="head">
         <Container>
           <Navbar.Brand href="#home">STYLISH</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link as={Link} to="home">Home</Nav.Link>
+            <Nav.Link as={Link} to="search">Search</Nav.Link>
             
           </Nav>
         </Container>
