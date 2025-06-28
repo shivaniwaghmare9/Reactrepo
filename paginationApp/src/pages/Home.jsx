1e1f581f67877c7ca4452b20e63077437c5fbdd2
shivani <shivaniw69@gmail.com>
2025-06-28 23:34:14 +0530

    home

--- paginationApp/src/pages/Home.jsx
+++ paginationApp/src/pages/Home.jsx
@@ -1,3 +1,17 @@
+//  import myimg from "../images/myimg6.jpg";
+
+// const Home=()=>{
+//     return(
+//         <>
+//         <h1 id="head">Home Page!!!</h1>
+//         <div>
+//          <img src={myimg} id="img"/>
+//         </div>
+//         </>
+//     )
+// }
+// export default Home;
+
  import myimg from "../images/myimg6.jpg";
 
 const Home=()=>{
