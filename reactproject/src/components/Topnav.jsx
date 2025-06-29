a14885fa26622839ff3696543aa1f475bf4f97f8
shivani <shivaniw69@gmail.com>
2025-06-29 20:54:01 +0530

    nav

--- reactproject/src/components/Topnav.jsx
+++ reactproject/src/components/Topnav.jsx
@@ -12,6 +12,9 @@ const Topnav=()=>{
           <Navbar.Brand href="#home"><img src={img4} alt="icon"  className='imgnav'/></Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link as={Link} to="home">Home</Nav.Link>
+            <Nav.Link as={Link} to="men">Men</Nav.Link>
+            <Nav.Link as={Link} to="women">women</Nav.Link>
+            <Nav.Link as={Link} to="kid">Kid</Nav.Link>
             <Nav.Link as={Link} to="search">Search</Nav.Link>
             
           </Nav>
