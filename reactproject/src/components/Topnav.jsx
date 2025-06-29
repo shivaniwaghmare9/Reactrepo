67b162532e828e8384d80b998da304436267baf2
shivani <shivaniw69@gmail.com>
2025-06-29 20:40:07 +0530

    css

--- reactproject/src/components/Topnav.jsx
+++ reactproject/src/components/Topnav.jsx
@@ -3,12 +3,13 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
+import img4 from "../images/images.jpeg"
 const Topnav=()=>{
     return(
         <>
          <Navbar bg="dark" data-bs-theme="dark" id="head">
         <Container>
-          <Navbar.Brand href="#home">STYLISH</Navbar.Brand>
+          <Navbar.Brand href="#home"><img src={img4} alt="icon"  className='imgnav'/></Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link as={Link} to="home">Home</Nav.Link>
             <Nav.Link as={Link} to="search">Search</Nav.Link>
