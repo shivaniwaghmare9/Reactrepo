44fb2cd52df7e98b8237bb93c85b80b0a3106450
shivani <shivaniw69@gmail.com>
2025-06-30 14:06:19 +0530

    headre

--- reactproject/src/components/Header.jsx
+++ reactproject/src/components/Header.jsx
@@ -1,4 +1,8 @@
 
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 import { FaShoppingCart } from "react-icons/fa";
 import { FaRegUserCircle } from "react-icons/fa";
@@ -7,11 +11,28 @@ const Header=()=>{
     return(
         <>
          <div className="header">
-             <div>
+            <div>
+            <Form inline>
+        <Row>
+          <Col xs="auto">
+            <Form.Control
+              type="text"
+              placeholder="Search"
+              className=" mr-sm-2"
+            />
+          </Col>
+          <Col xs="auto">
+            <Button type="submit">Submit</Button>
+          </Col>
+        </Row>
+      </Form>
+    </div>
+    <div>
+        
               <h4><FaRegUserCircle /> <br/>profile</h4>
               <h4><FaShoppingCart /><br/>cart</h4> 
-             </div>
              
+    </div> 
             
              
            </div>
