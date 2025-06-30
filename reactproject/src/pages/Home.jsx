d1bc98fe123a393ff195dcdb807dcc92ee623298
shivani <shivaniw69@gmail.com>
2025-06-30 23:26:20 +0530

    css

--- reactproject/src/pages/Home.jsx
+++ reactproject/src/pages/Home.jsx
@@ -58,7 +58,7 @@ const Home=()=>{
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item>
+      <Carousel.Item >
         <img src={img3} alt="web" width="100%" height="600px"/>
         <Carousel.Caption>
           <h3>Third slide label</h3>
