94090263193c5b9ff225c544300fb8876ac65943
shivani <shivaniw69@gmail.com>
2025-07-01 01:13:12 +0530

    slice

--- reactproject/src/cartSlice.jsx
+++ reactproject/src/cartSlice.jsx
@@ -1,1 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
 
+
+const cartSlice=createSlice({
+    name:"mycart",
+    initialState:{
+        cart:[]
+    },
+    reducers:{
+        addCart:(state,actions)=>{
+            state.cart.filter((key)=>key.id==actions.payload.id)
+        }
+    }
+})
+export const{addCart}=cartSlice.actions;
+export default cartSlice.reducer;
