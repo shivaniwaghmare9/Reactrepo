42eb2ac725620151c54312fccb316b9275a48842
shivani <shivaniw69@gmail.com>
2025-07-01 23:52:53 +0530

    card

--- reactproject/src/pages/Men.jsx
+++ reactproject/src/pages/Men.jsx
@@ -12,6 +12,24 @@ const Men=()=>{
     useEffect(()=>{
         loadData()
     },[])
+    const ans=mydata.map((key)=>{
+        return(
+            <>
+    <Card id="card">
+        <Card.Img variant="top" src={key.image}  id="img"/>
+      <Card.Body>
+        <Card.Title>Name:{key.name}</Card.Title>
+        <Card.Text>
+          Brand:{key.brand}<br/>
+          Category:{key.category}<br/>
+          Price:{key.price}<br/>
+        </Card.Text>
+        <Button variant="primary" onClick={()=>{dispatch(addCart({id:key.id,name:key.name,image:key.image,brand:key.brand,category:key.category,price:key.price}))}} className='cartbtn'>Add To Cart</Button>
+      </Card.Body>
+    </Card>
+            </>
+        )
+    })
     return(
         <>
         </>
