48aa59c8eccd99093edb1e77efadf0b23e01739e
shivani <shivaniw69@gmail.com>
2025-07-03 22:21:01 +0530

    main

--- reactappp/src/main.jsx
+++ reactappp/src/main.jsx
@@ -1,4 +1,17 @@
 
+// import { createRoot } from 'react-dom/client'
+
+// import App from './App.jsx'
+// import store from './store.jsx'
+// import { Provider } from 'react-redux'
+// createRoot(document.getElementById('root')).render(
+//   <Provider store={store}>
+//       <App />
+//   </Provider>
+   
+// )
+
+
 import { createRoot } from 'react-dom/client'
 
 import App from './App.jsx'
@@ -10,3 +23,4 @@ createRoot(document.getElementById('root')).render(
   </Provider>
    
 )
+
