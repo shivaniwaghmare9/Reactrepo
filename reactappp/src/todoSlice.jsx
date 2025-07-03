7eed0129b4f366ddc9e80a2427e4bf6d18bfefa3
shivani <shivaniw69@gmail.com>
2025-07-03 22:21:53 +0530

    slise

--- reactappp/src/todoSlice.jsx
+++ reactappp/src/todoSlice.jsx
@@ -1,3 +1,63 @@
+// import { createSlice } from "@reduxjs/toolkit";
+
+// const todoSlice=createSlice({
+//     name:"todo",
+//     initialState:{
+//         task:[]
+//     },
+//     reducers:{
+//         addTask:(state,actions)=>{
+//             console.log(actions)
+//             state.task.push(actions.payload)
+//         },
+//         // taskDelete:(state,actions)=>{
+//          //console.log(actions.payload)
+//         //     state.task=state.task.filter(items=>items.id!=actions.payload.id)
+//         // }
+//         taskDelete:(state,actions)=>{
+//             console.log(actions.payload.id)
+//             state.task=state.task.filter(function(key){
+//                 if(key.id==actions.payload.id)
+//                 {
+//                     return false
+//                 }
+//                 else{
+//                     return true;
+//                 }
+//             })
+//         },
+//         taskRebyindex:(state,actions)=>{
+//             console.log(actions.payload.id)
+//          state.task.splice(actions.payload.id,1)
+//         },
+//         taskComplete:(state,actions)=>{
+//             for(var i=0; i<state.task.length; i++)
+//             if(state.task[i].id==actions.payload.id)
+//             {
+//                 state.task[i].taskStatus=true;
+//             }
+//         },
+//         taskInComplete:(state,actions)=>{
+//             for(var i=0; i<state.task.length; i++)
+//             if(state.task[i].id==actions.payload.id)
+//             {
+//                 state.task[i].taskStatus=false;
+//             }
+//         },
+//         taskEdit:(state,actions)=>{
+//             for(var i=0; i<state.task.length; i++)
+//             {
+//                 if(state.task[i].id==actions.payload.id)
+//                 {
+//                     state.task[i].work=actions.payload.work;
+//                 }
+//             }
+//         }
+//     }
+// })
+// export const{addTask,taskDelete,taskRebyindex,taskComplete,taskInComplete,taskEdit}=todoSlice.actions;
+// export default todoSlice.reducer;
+
 import { createSlice } from "@reduxjs/toolkit";
 
 const todoSlice=createSlice({
