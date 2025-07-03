199ff92e69bed336455b18cc40557dc193e27b00
shivani <shivaniw69@gmail.com>
2025-07-03 22:21:27 +0530

    store

--- reactappp/src/store.jsx
+++ reactappp/src/store.jsx
@@ -1,3 +1,12 @@
+// import { configureStore } from "@reduxjs/toolkit";
+// import myReducer from "./todoSlice"
+// const store=configureStore({
+//     reducer:{
+//       todo:myReducer
+//     }
+// })
+// export default store;
+
 import { configureStore } from "@reduxjs/toolkit";
 import myReducer from "./todoSlice"
 const store=configureStore({
