dcfd42ce354b4e4ea7365565467cf149bae6309d
shivani <shivaniw69@gmail.com>
2025-07-05 02:12:44 +0530

    data

--- reactproject/src/pages/Women.jsx
+++ reactproject/src/pages/Women.jsx
@@ -34,14 +34,48 @@ const Women=()=>{
         <Button variant="primary" onClick={()=>{dispatch(addCart({id:key.id,name:key.name,image:key.image,brand:key.brand,category:key.category,qnty:1,price:key.price}))}} className='cartbtn'>Add To Cart</Button>
       </Card.Body>
     </Card>
-            </>
+     </>
         )
     })
+
+const[myData,setMyData]=useState([]);
+    
+    const loaddata=async()=>{
+        let api="http://localhost:3000/products/?category=women"
+        const response=await axios.get(api);
+        console.log(response.data);
+        setMydata(response.data);
+    }
+    useEffect(()=>{
+        loaddata()
+    },[])
+    const ans2=myData.map((key)=>{
+        return(
+            <>
+    <Card id="card">
+        <Card.Img variant="top" src={key.image}  id="img"/>
+      <Card.Body>
+        <Card.Title>Name:{key.name}</Card.Title>
+        <Card.Text>
+          Brand:{key.brand}<br/>
+          Category:{key.category}<br/>
+          Price:{key.price}<br/>
+        </Card.Text>
+        <Button variant="primary" onClick={()=>{dispatch(addCart({id:key.id,name:key.name,image:key.image,brand:key.brand,category:key.category,qnty:1,price:key.price}))}} className='cartbtn'>Add To Cart</Button>
+      </Card.Body>
+    </Card>
+     </>
+        )
+    })
+
     return(
         <>
         <h3 id="h3tag">Women Latest Shoes</h3>
          <div id="carddata">
            {ans}
+        </div>
+        <div id="carddata">
+           {ans2}
         </div>
         </>
     )
