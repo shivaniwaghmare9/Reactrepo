d72c36118007a325e92ad510ad852ed3cc89d4a9
shivani <shivaniw69@gmail.com>
2025-07-05 02:54:13 +0530

    insert

--- reactappp/src/pages/Insert.jsx
+++ reactappp/src/pages/Insert.jsx
@@ -1,4 +1,5 @@
 
+import axios from "axios";
 import { useState } from "react";
 const Insert=()=>{
     const[input,setInput]=useState({})
@@ -6,7 +7,13 @@ const Insert=()=>{
         let name=e.target.name;
         let value=e.taeget.value;
         console.log(input)
-        setInput(values=>{...values,[name]:value})
+        setInput(values=>({...values,[name]:value}))
+    }
+    const handleSubmit=async()=>{
+        let api="http://localhost:3000/data";
+        const response=await axios.push(api)
+        console.log(response.data)
+        setInput(response.data)
     }
     return(
         <>
@@ -16,7 +23,7 @@ const Insert=()=>{
             Enter Rollno: <input type="text" name="rollno" onChange={handleInput}/><br/><br/>
             Enter City: <input type="text" name="city" onChange={handleInput}/><br/><br/>
             Enter Fees: <input type="text" name="fees" onChange={handleInput}/><br/><br/>
-            <button>Submit</button>
+            <button onClick={handleSubmit}>Submit</button>
          </div>
         </>
     )
