3c8c352162c838392ed428b4fcc6a1e31beeb672
shivani <shivaniw69@gmail.com>
2025-07-06 01:18:51 +0530

    table

--- reactappp/src/pages/Display.jsx
+++ reactappp/src/pages/Display.jsx
@@ -1,4 +1,5 @@
 
+import Table from 'react-bootstrap/Table';
 import axios from "axios";
 import { useState,useEffect } from "react";
 const Display=()=>{
@@ -12,8 +13,38 @@ const Display=()=>{
     useEffect(()=>{
         loadData()
     },[])
+    let sno=0;
+    const ans=mydata.map((key)=>{
+        sno++;
+        return(
+            <>
+            <tr>
+                <td>{sno}</td>
+                <td>{key.name}</td>
+                <td>{key.rollno}</td>
+                <td>{key.city}</td>
+                <td>{key.fees}</td>
+            </tr>
+            </>
+        )
+    })
     return(
         <>
+           <h3>Display data!!</h3>
+           <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>Sno</th>
+          <th>Name</th>
+          <th>Rollno</th>
+          <th>City</th>
+          <th>Fees</th>
+        </tr>
+      </thead>
+      <tbody>
+        {ans}
+      </tbody>
+      </Table>
         </>
     )
 }
