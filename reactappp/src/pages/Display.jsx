1138c7eb701df71b97f58e7d1c74750851497250
shivani <shivaniw69@gmail.com>
2025-07-06 01:15:44 +0530

    dsiplay

--- reactappp/src/pages/Display.jsx
+++ reactappp/src/pages/Display.jsx
@@ -1,1 +1,20 @@
 
+import axios from "axios";
+import { useState,useEffect } from "react";
+const Display=()=>{
+    const [mydata,setMydata]=useState([]);
+    const loadData=async()=>{
+        let api="http://localhost:3000/data"
+       const response=await axios.get(api);
+       setMydata(response.data);
+       console.log(response.data);
+    }
+    useEffect(()=>{
+        loadData()
+    },[])
+    return(
+        <>
+        </>
+    )
+}
+export default Display;
