fc33f2960c788bf5c1d7006782307cf76881f73e
shivani <shivaniw69@gmail.com>
2025-07-06 01:30:45 +0530

    search

---
+++ reactappp/src/pages/Search.jsx
@@ -0,0 +1,36 @@
+
+import axios from "axios";
+import { useState } from "react";
+const Search=()=>{
+    const[empno,setEmpno]=useState("");
+    const[mydata,setMydata]=useState([]);
+    const handleSubmit=async()=>{
+        let api=`http://localhost:3000/data/?rollno=${empno}`;
+        const response=await axios.get(api);
+        setMydata(response.data);
+        console.log(response.data);
+    }
+    let sno=0;
+    const ans=mydata.map((key)=>{
+        sno++;
+        return(
+            <>
+            <tr>
+                <td>{sno}</td>
+                <td>{key.name}</td>
+                <td>{key.rollno}</td>
+                <td>{key.city}</td>
+                <td>{key.fees}</td>
+            </tr>
+            </>
+        )
+    })
+    return(
+        <>
+        <h3>Search data!!!</h3>
+        Enter Rollno: <input type="text" value={empno} onChange={(e)=>{setEmpno(e.target.value)}}/><br/><br/>
+        <button onClick={handleSubmit}>Search</button>
+        </>
+    )
+}
+export default Search;
