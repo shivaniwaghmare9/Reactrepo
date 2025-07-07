9cdab89b4ad2458da3e9a4b762a5b592bf348dc3
shivani <shivaniw69@gmail.com>
2025-07-08 01:44:34 +0530

    first commit

---
+++ reactproject/src/pages/ProductShow.jsx
@@ -0,0 +1,32 @@
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+const ProductShow = () => {
+    const { id } = useParams();
+    const [mydata, setMyData] = useState({})
+    const loadData = async () => {
+        let api = `http://localhost:3000/products/${id}`;
+        const response = await axios.get(api);
+        setMyData(response.data);
+        console.log(response.data);
+    }
+    useEffect(() => {
+        loadData();
+    }, []);
+    return (
+        <>
+            <div id="productShow">
+                <div>
+                    <img src={mydata.image} width="300" height="300" />
+                </div>
+                <div>
+                    <h1> Product Name : {mydata.name}</h1> 
+                    <h3 style={{color:"green"}}> Category : {mydata.category}</h3>
+                    <h2 style={{color:"red"}}> Price  : {mydata.price}</h2>
+                     </div>
+            </div>
+
+        </>
+    )
+}
+export default ProductShow;
