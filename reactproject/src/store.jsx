ba5a8a1f875c45c3ee8b358f3d285f54ec4b1908
shivani <shivaniw69@gmail.com>
2025-07-08 23:23:35 +0530

    commit

--- reactproject/src/store.jsx
+++ reactproject/src/store.jsx
@@ -1,8 +1,18 @@
+
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import myReducer from "./cartSlice"
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+const persistedReducer = persistReducer(persistConfig, myReducer);
 const store=configureStore({
     reducer:{
-     mycart:myReducer
+     mycart:persistedReducer
     }
 })
 export default store;
+export const persistor = persistStore(store);
