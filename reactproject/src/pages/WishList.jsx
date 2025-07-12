ab29829ba19d6ce7f7b59e996183c397d94e83d3
shivani <shivaniw69@gmail.com>
2025-07-13 02:36:47 +0530

    wish

--- reactproject/src/pages/WishList.jsx
+++ reactproject/src/pages/WishList.jsx
@@ -1,25 +1,24 @@
 import Table from 'react-bootstrap/Table';
 import { useSelector,useDispatch } from "react-redux";
 import { ReMove } from '../wishSlice';
-import { useNavigate } from "react-router-dom";
+
 
 const Wishlist=()=>{
      const wish=useSelector(state=>state.mycardd.cardd);
      const dispatch=useDispatch();
-    const ans=wish.map((key)=>{
+     const ans=wish.map((key)=>{
          
         return(
             <>
              <tr>
+              
                 <td>
                 <img src={key.image} alt="web" width="100px" height="100px"/>
                 </td>
                 <td>{key.name}</td>
                 <td>{key.brand}</td>
                 <td>{key.category}</td>
-                
                 <td>{key.price}</td>
-                 
                 <td>
                   <span onClick={()=>{dispatch(ReMove({id:key.id}))}} style={{backgroundColor:"black",padding:"0.4rem",color:"aliceblue"}}>remove</span>
                 </td>
@@ -38,7 +37,6 @@ const Wishlist=()=>{
           <th>Name</th>
           <th>Brand</th>
           <th>Category</th>
-          <th>Quantity</th>
           <th>Price</th>
           <th>Remove</th>
         </tr>
