39e68baf8d52341e1abc974927f0e415b5c1301b
shivani <shivaniw69@gmail.com>
2025-07-13 22:09:09 +0530

    nav

--- test2app/src/Navbar.jsx
+++ test2app/src/Navbar.jsx
@@ -12,10 +12,12 @@ const Topbar=()=>{
          <Container>
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
           <Nav className="me-auto">
+
+
             <Nav.Link as={Link} to="home">Home</Nav.Link>
-            <Nav.Link as={Link} to="style">Theme Change App</Nav.Link>
-            <Nav.Link as={Link} to="about">Counter App</Nav.Link>
-            <Nav.Link as={Link} to="faculty">Todo App</Nav.Link>
+            <Nav.Link as={Link} to="theme">Theme Change App</Nav.Link>
+            <Nav.Link as={Link} to="counter">Counter App</Nav.Link>
+            <Nav.Link as={Link} to="todo">Todo App</Nav.Link>
             <Nav.Link as={Link} to="contact">Contact Us</Nav.Link>
             
           </Nav>
---
+++ test2app/src/Pages/Home.jsx
@@ -0,0 +1,9 @@
+
+const Home=()=>{
+    return(
+        <>
+        <h3>This is our home page</h3>
+        </>
+    )
+}
+export default Home;
