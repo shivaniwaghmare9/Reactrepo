cd52e70f8fafdcc80a4892dd3e8c84885ea69624
shivani <shivaniw69@gmail.com>
2025-07-13 22:02:36 +0530

    gfd

--- test2app/src/App.jsx
+++ test2app/src/App.jsx
@@ -1,11 +1,12 @@
 
 import { BrowserRouter,Routes,Route } from "react-router-dom";
+import Layout from "./Layout";
 const App=()=>{
   return(
     <>
       <BrowserRouter>
          <Routes>
-            <Route path="/" element={<}>
+            <Route path="/" element={<Layout/>}>
 
             </Route>
          </Routes>
---
+++ test2app/src/Layout.jsx
@@ -0,0 +1,10 @@
+import Navbar from "./Navbar";
+
+const Layout=()=>{
+    return(
+        <>
+        <Navbar/>
+        </>
+    )
+}
+export default Layout;
