6fc05199410efd57d70a3ac8010957ed97fa37f0
shivani <shivaniw69@gmail.com>
2025-07-13 22:06:02 +0530

    asd

--- test2app/src/Navbar.jsx
+++ test2app/src/Navbar.jsx
@@ -5,7 +5,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
 
-const Navbar=()=>{
+const Topbar=()=>{
     return(
         <>
         <Navbar bg="dark" data-bs-theme="dark">
@@ -13,10 +13,9 @@ const Navbar=()=>{
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link as={Link} to="home">Home</Nav.Link>
-            <Nav.Link as={Link} to="style">Style</Nav.Link>
-            <Nav.Link as={Link} to="about">About</Nav.Link>
-            <Nav.Link as={Link} to="faculty">Faculty</Nav.Link>
-            <Nav.Link as={Link} to="course">OurCourse</Nav.Link>
+            <Nav.Link as={Link} to="style">Theme Change App</Nav.Link>
+            <Nav.Link as={Link} to="about">Counter App</Nav.Link>
+            <Nav.Link as={Link} to="faculty">Todo App</Nav.Link>
             <Nav.Link as={Link} to="contact">Contact Us</Nav.Link>
             
           </Nav>
@@ -25,4 +24,4 @@ const Navbar=()=>{
         </>
     )
 }
-export default Navbar;
+export default Topbar;
