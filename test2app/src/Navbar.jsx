db89d3b13e654a7681ce6315b00e11e60ebedb36
shivani <shivaniw69@gmail.com>
2025-07-13 22:00:30 +0530

    ads

--- test2app/src/Navbar.jsx
+++ test2app/src/Navbar.jsx
@@ -4,11 +4,12 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
+
 const Navbar=()=>{
     return(
         <>
-          <Navbar bg="dark" data-bs-theme="dark">
-        <Container>
+        <Navbar bg="dark" data-bs-theme="dark">
+         <Container>
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link as={Link} to="home">Home</Nav.Link>
