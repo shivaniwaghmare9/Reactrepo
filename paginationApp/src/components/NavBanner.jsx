2274561bb13fe527125e79a08b221c99e3f1c172
shivani <shivaniw69@gmail.com>
2025-07-15 23:24:31 +0530

    nav

--- paginationApp/src/components/NavBanner.jsx
+++ paginationApp/src/components/NavBanner.jsx
@@ -1,18 +1,4 @@
 
-// import myimg from "../images/myimg6.jpg";
-
-// const NavBanner=()=>{
-//     return(
-//         <>
-//          <div>
-//          <img src={myimg} id="img"/>
-//         </div>
-//         </>
-//     )
-// }
-// export default NavBanner;
-
-
 import myimg from "../images/myimg6.jpg";
 
 const NavBanner=()=>{
