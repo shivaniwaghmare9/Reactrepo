feb3284cedb3299e109fde3a2dec3c97b5ead8b8
shivani <shivaniw69@gmail.com>
2025-07-16 23:34:36 +0530

    store

--- reactproject/src/store.jsx
+++ reactproject/src/store.jsx
@@ -1,4 +1,27 @@
 
+// import { configureStore } from "@reduxjs/toolkit";
+// import { persistStore, persistReducer } from "redux-persist";
+// import storage from "redux-persist/lib/storage";
+// import myReducer from "./cartSlice"
+// import wishReducer from "./wishSlice"
+
+// const persistConfig = {
+//   key: "root",
+//   storage,
+// };
+
+// const persistedReducer = persistReducer(persistConfig, myReducer);
+// const persistedReducerr = persistReducer(persistConfig, wishReducer);
+
+// const store=configureStore({
+//     reducer:{
+//      mycard:persistedReducer,
+//      mycardd:persistedReducerr
+//     }
+// })
+// export default store;
+// export const persistor = persistStore(store);
+
 import { configureStore } from "@reduxjs/toolkit";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
