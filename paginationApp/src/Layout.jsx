0f40f15cd12b6b5e105b8aa23bfe397e124811a3
shivani <shivaniw69@gmail.com>
2025-07-16 23:12:58 +0530

    layout

--- paginationApp/src/Layout.jsx
+++ paginationApp/src/Layout.jsx
@@ -1,17 +1,3 @@
-// import { Outlet } from "react-router-dom";
-// import Topnav from "./components/Topnav";
-// import Footer from "./components/Footer";
-
-// const Layout=()=>{
-//     return(
-//         <>
-//          <Topnav/>
-//          <Outlet/>
-//          <Footer/>
-//         </>
-//     )
-// }
-// export default Layout;
 import { Outlet } from "react-router-dom";
 import Topnav from "./components/Topnav";
 import Footer from "./components/Footer";
