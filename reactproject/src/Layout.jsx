60fed123489857de441bfc183b08d248c3502b0a
shivani <shivaniw69@gmail.com>
2025-07-16 23:33:54 +0530

    layout

--- reactproject/src/Layout.jsx
+++ reactproject/src/Layout.jsx
@@ -1,4 +1,26 @@
 
+// import { Outlet } from "react-router-dom";
+// import Topnav from "./components/Topnav";
+// import Footer from "./components/Footer";
+// import Header from "./components/Header";
+
+// const Layout=()=>{
+//     return(
+//         <>
+//         <div id="topHeader">
+//          <Header/>
+//          <Topnav/>
+//         </div>
+//          <div>
+//             <Outlet/>
+//          </div>
+         
+//          <Footer/>
+//         </>
+//     )
+// }
+// export default Layout;
+
 import { Outlet } from "react-router-dom";
 import Topnav from "./components/Topnav";
 import Footer from "./components/Footer";
