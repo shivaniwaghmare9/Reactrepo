480bfe1f3a82ceb31d393809c59a5f09cb88dcfc
shivani <shivaniw69@gmail.com>
2025-07-18 22:43:47 +0530

    hfg

--- reactappp/src/pages/Display.jsx
+++ reactappp/src/pages/Display.jsx
@@ -1,4 +1,55 @@
 
+// import Table from 'react-bootstrap/Table';
+// import axios from "axios";
+// import { useState,useEffect } from "react";
+// const Display=()=>{
+//     const [mydata,setMydata]=useState([]);
+//     const loadData=async()=>{
+//         let api="http://localhost:3000/data"
+//        const response=await axios.get(api);
+//        setMydata(response.data);
+//        console.log(response.data);
+//     }
+//     useEffect(()=>{
+//         loadData()
+//     },[])
+//     let sno=0;
+//     const ans=mydata.map((key)=>{
+//         sno++;
+//         return(
+//             <>
+//             <tr>
+//                 <td>{sno}</td>
+//                 <td>{key.name}</td>
+//                 <td>{key.rollno}</td>
+//                 <td>{key.city}</td>
+//                 <td>{key.fees}</td>
+//             </tr>
+//             </>
+//         )
+//     })
+//     return(
+//         <>
+//            <h3>Display data!!</h3>
+//            <Table striped bordered hover>
+//       <thead>
+//         <tr>
+//           <th>Sno</th>
+//           <th>Name</th>
+//           <th>Rollno</th>
+//           <th>City</th>
+//           <th>Fees</th>
+//         </tr>
+//       </thead>
+//       <tbody>
+//         {ans}
+//       </tbody>
+//       </Table>
+//         </>
+//     )
+// }
+// export default Display;
+
 import Table from 'react-bootstrap/Table';
 import axios from "axios";
 import { useState,useEffect } from "react";
