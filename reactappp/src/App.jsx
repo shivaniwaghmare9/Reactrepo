63cbe8d2d2f9b0512bdfbf765f779a9984478ace
shivani <shivaniw69@gmail.com>
2025-07-18 22:49:25 +0530

    app

--- reactappp/src/App.jsx
+++ reactappp/src/App.jsx
@@ -1,32 +1,3 @@
-// import { BrowserRouter,Routes,Route } from "react-router-dom";
-// import Layout from "./Layout";
-// import Home from "./pages/Home";
-// import Insert from "./pages/Insert";
-// import Display from "./pages/Display";
-// import Search from "./pages/Search";
-// import Update from "./pages/Update";
-// import Myedit from "./pages/MyEdit";
-// const App=()=>{
-//   return(
-//     <>
-//      <BrowserRouter>
-//        <Routes>
-//         <Route path="/" element={<Layout/>}>
-//         <Route index element={<Home/>}/>
-//         <Route path="home" element={<Home/>}/>
-//         <Route path="insert" element={<Insert/>}/>
-//         <Route path="display" element={<Display/>}/>
-//         <Route path="search" element={<Search/>}/>
-//         <Route path="update" element={<Update/>}/>
-//         <Route path="edit/:id" element={<Myedit/>}/>
-//         </Route>
-//        </Routes>
-//      </BrowserRouter>
-//     </>
-//   )
-// }
-// export default App;
-
 import { BrowserRouter,Routes,Route } from "react-router-dom";
 import Layout from "./Layout";
 import Home from "./pages/Home";
@@ -55,4 +26,3 @@ const App=()=>{
   )
 }
 export default App;
-
