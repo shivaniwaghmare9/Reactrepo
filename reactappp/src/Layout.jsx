49fe899668bd99867f6b98ca252389e5adc70227
shivani <shivaniw69@gmail.com>
2025-07-18 22:50:13 +0530

    lay

--- reactappp/src/Layout.jsx
+++ reactappp/src/Layout.jsx
@@ -1,16 +1,4 @@
 
-// import { Outlet } from "react-router-dom";
-// import Topnav from "./TopNav";
-// const Layout=()=>{
-//     return(
-//         <>
-//         <Topnav/>
-//         <Outlet/>
-//         </>
-//     )
-// }
-// export default Layout;
-
 import { Outlet } from "react-router-dom";
 import Topnav from "./TopNav";
 const Layout=()=>{
