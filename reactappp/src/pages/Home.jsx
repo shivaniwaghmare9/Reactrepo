0e27d144dfcdad1b6a0092f28adb17d3c799be1c
shivani <shivaniw69@gmail.com>
2025-07-18 22:44:44 +0530

    home

--- reactappp/src/pages/Home.jsx
+++ reactappp/src/pages/Home.jsx
@@ -1,4 +1,14 @@
 
+// const Home=()=>{
+//     return(
+//         <>
+//         <h3>This is our home page</h3>
+//         </>
+//     )
+// }
+// export default Home;
+
+
 const Home=()=>{
     return(
         <>
