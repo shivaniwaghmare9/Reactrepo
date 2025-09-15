236ed29f4e372dcc8a0a4ec1c5f7de9bb13c7064
shivani <shivaniw69@gmail.com>
2025-09-15 22:04:33 +0530

    header

--- reactproject/src/components/Header.jsx
+++ reactproject/src/components/Header.jsx
@@ -1,4 +1,39 @@
 
+// import Button from 'react-bootstrap/Button';
+// import { useState } from 'react';
+// import { FaShoppingCart } from "react-icons/fa";
+// import { FaRegUserCircle } from "react-icons/fa";
+// import { useSelector } from 'react-redux';
+// import { useNavigate } from 'react-router-dom';
+// const Header=()=>{
+//     const navigate=useNavigate();
+//     const cartData=useSelector(state=>state.mycard.card);
+//     const cartLength=cartData.length;    
+//     const [txtdata, setTxtData]= useState("");
+
+//     const handleSubmit=()=>{
+//     navigate(`searchdata/${txtdata}`)
+//   }
+//     return(
+//         <>
+//     <div className="header">
+//          <div id="inptag">
+//         <input type="text" placeholder="Search" className="inp" name="txtval" value={txtdata} 
+//         onChange={(e)=>{setTxtData(e.target.value)}}/>
+//         <Button type="submit" id="subbtn" onClick={handleSubmit}>Search</Button>
+//        </div>
+//             <div className='icons'>
+        
+//               <h5><FaRegUserCircle /> <br/>profile</h5>
+//               <h5><span style={{color:"indianred"}}>{cartLength}</span><br/><FaShoppingCart  id="carticon" onClick={()=>{navigate("/cartdata")}}/></h5> 
+             
+//             </div> 
+//     </div>
+//         </>
+//     )
+// }
+// export default Header;
+
 import Button from 'react-bootstrap/Button';
 import { useState } from 'react';
 import { FaShoppingCart } from "react-icons/fa";
@@ -33,3 +68,4 @@ const Header=()=>{
     )
 }
 export default Header;
+
