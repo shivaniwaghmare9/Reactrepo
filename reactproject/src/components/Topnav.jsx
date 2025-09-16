0e00ce4c89bd25ffbcd08ef73d95cb97a6bf676f
shivani <shivaniw69@gmail.com>
2025-09-16 22:10:55 +0530

    nav

--- reactproject/src/components/Topnav.jsx
+++ reactproject/src/components/Topnav.jsx
@@ -1,4 +1,35 @@
 
+// import Container from 'react-bootstrap/Container';
+// import Nav from 'react-bootstrap/Nav';
+// import Navbar from 'react-bootstrap/Navbar';
+// import { Link } from 'react-router-dom';
+// import img4 from "../images/images.jpeg"
+// const Topnav=()=>{
+//     return(
+//         <>
+//          <Navbar bg="light" data-bs-theme="light" id="head">
+//         <Container>
+//           <Navbar.Brand href="#home"><img src={img4} alt="icon"  className='imgnav'/></Navbar.Brand>
+//           <Nav className="me-auto w-100 d-flex justify-content-center align-items-center">
+//             <Nav.Link as={Link} to="home" id="content1">Home</Nav.Link>
+//             <Nav.Link as={Link} to="men" id="content">Men</Nav.Link>
+//             <Nav.Link as={Link} to="women" id="content">Women</Nav.Link>
+//             <Nav.Link as={Link} to="kid" id="content">Kid</Nav.Link>
+//             <Nav.Link as={Link} to="sortData" id="content">SortData</Nav.Link>
+//             <Nav.Link as={Link} to="cartdata" id="content">CartData</Nav.Link>
+//             <Nav.Link as={Link} to="search" id="content">Search</Nav.Link>
+//             <Nav.Link as={Link} to="wishlist" id="content">Wishlist</Nav.Link>
+            
+        
+          
+//          </Nav>
+//         </Container>
+//       </Navbar>
+//         </>
+//     )
+// }
+// export default Topnav;
+
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
