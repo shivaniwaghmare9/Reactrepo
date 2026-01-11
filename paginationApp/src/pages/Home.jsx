c0c54ec125887c331f0d9ed2119562aeea7f3457
shivani <shivaniw69@gmail.com>
2026-01-11 21:17:01 +0530

    ,...,...

--- paginationApp/src/pages/Home.jsx
+++ paginationApp/src/pages/Home.jsx
@@ -3,7 +3,7 @@
 const Home=()=>{
     return(
         <>
-        <h1 id="head">Home Page!!!</h1>
+        <h1 id="head"> Home Page !!!</h1>
         <div>
          <img src={myimg} id="img"/>
         </div>
